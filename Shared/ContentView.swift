f4b73a1737aaa6f732f70836f7ee5adec139eeba
Osama Chatbi <o.chatbi@ntnext.it>
2022-03-16 10:53:38 +0100

    Rx Call

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -6,16 +6,32 @@
 //
 
 import SwiftUI
+import RxSwift
 
 struct ContentView: View {
+    @ObservedObject var updater = DelayedUpdater()
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Text("Hello, world!").padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+
+    }
+}
+
+@MainActor class DelayedUpdater: ObservableObject {
+    init() {
+        ViewModel().loadMovieDetail(movieId: "222").subscribe { movieResp in
+            print(movieResp.results)
+        } onError: { error in
+            print("2")
+        } onCompleted: {
+            print("Completed")
+        } onDisposed: {
+            DisposeBag()
+        }
     }
 }
